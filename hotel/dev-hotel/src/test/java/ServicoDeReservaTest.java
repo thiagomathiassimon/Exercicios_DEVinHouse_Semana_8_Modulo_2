5db59241cdc35eba3373eeaa8a456f8ff805b239
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-05-10 20:53:07 -0300

    Exercicio_11

--- hotel/dev-hotel/src/test/java/ServicoDeReservaTest.java
+++ hotel/dev-hotel/src/test/java/ServicoDeReservaTest.java
@@ -1,13 +1,27 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServicoDeReservaTest {
 
-    @Test
-    void Teste(){
-        assertEquals(1,1);
+    ServicoDeReserva servicoDeReserva;
+
+    @BeforeEach
+    public void setup(){
+        servicoDeReserva = mock(ServicoDeReserva.class);
     }
 
+    @Test
+    void Deve_Testar_Se_O_Metodo_geQuantidadeDisponivel_Possui_Comportamento_De_Nice_Mock(){
+        int actual = servicoDeReserva.getQuantidadeDisponivel();
+
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    
 
 }
